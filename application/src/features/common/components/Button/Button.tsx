e96a5d97e25843e48229d7d4d8c8cedc38cb888b
kaneko1117 <yuta_kaneko@pj100.biz>
2024-06-25 09:24:09 +0900

    ✨propsの命名をlabelにしないと、storybookに反映しなかった

--- application/src/features/common/components/Button/Button.tsx
+++ application/src/features/common/components/Button/Button.tsx
@@ -2,10 +2,10 @@ import { ReactNode } from "react";
 import { Button as ShadcnButton } from "@/shadcn-ui/button";
 
 type Props = {
-  children: ReactNode;
+  label: ReactNode;
   variant?: "default" | "outline";
 };
 
-export const Button = ({ children, variant = "default" }: Props) => (
-  <ShadcnButton variant={variant}>{children}</ShadcnButton>
+export const Button = ({ label, variant = "default" }: Props) => (
+  <ShadcnButton variant={variant}>{label}</ShadcnButton>
 );
