20e06854767e65c28356797c4b2d417cc323b0fa
kaneko1117 <yuta_kaneko@pj100.biz>
2024-06-28 10:08:41 +0900

    ✨footerの年を動的に変更

--- application/src/features/common/components/Footer/index.tsx
+++ application/src/features/common/components/Footer/index.tsx
@@ -1,7 +1,9 @@
+const year = new Date().getFullYear();
+
 export const Footer = () => {
   return (
     <footer className="text-text sticky top-[100vh] w-full bg-primary h-[40px] flex justify-center items-center">
-      © 2024 Aiful Digital Department
+      © {year} Aiful Digital Department
     </footer>
   );
 };
