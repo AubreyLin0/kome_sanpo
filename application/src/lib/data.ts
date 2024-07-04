a50a33f73c72a8ea46ace83a2b4284a5f417c15e
kaneko1117 <yuta_kaneko@pj100.biz>
2024-07-04 15:40:23 +0900

    ✨コメントアウト

--- application/src/lib/data.ts
+++ application/src/lib/data.ts
@@ -78,11 +78,6 @@ const sampleRestaurantData: CardDataType = [
 
 export const getRestaurantData = async () => {
   // todo:listデータ取得処理を実装する
-  // 重い処理
-  for (let i = 0; i < 100000; i += 1) {
-    // do nothing
-    console.log(i);
-  }
   noStore();
   return sampleRestaurantData;
 };
