4dd0315ad84c251f80c179eff8388e94c92fbb81
kaneko1117 <yuta_kaneko@pj100.biz>
2024-07-09 22:20:14 +0900

    ✨カードをLink化

--- application/src/features/common/components/Card/ResponsiveCard.tsx
+++ application/src/features/common/components/Card/ResponsiveCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { clsx } from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import { CardDataType } from "../../type";
 import { Button } from "../Button";
 import { LikeButton } from "../LikeButton";
@@ -18,54 +19,56 @@ type Props = {
 };
 
 export const ResponsiveCard = ({ data }: Props) => {
-  return data.map((item) => (
-    <ShadcnCard key={item.title} className="border-b-2 text-sm">
-      <CardHeader>
-        <CardTitle>{item.title}</CardTitle>
-      </CardHeader>
-      <div className="grid grid-cols-[1fr_120px] pb-4 gap-4">
-        <div className="flex flex-col justify-between">
-          <CardContent>
-            <div className="flex items-center gap-2">
-              <Button disabled={true} variant="outline" size="xs">
-                {item.category}
-              </Button>
-              <p className="text-subText">{item.distance}</p>
-            </div>
-          </CardContent>
-          <CardFooter className="w-full pb-0">
-            <div className="flex items-center justify-between gap-2 w-full">
-              <p
-                className={clsx("font-bold", {
-                  "text-green": item.isOpen,
-                  "text-red": !item.isOpen,
-                })}
-              >
-                {item.isOpen ? "営業中" : "営業時間外"}
-                <br />
-                <span className="text-text font-normal">
-                  {item.isOpen ? "営業終了" : "営業開始"}：{item.openingTime}
-                </span>
-              </p>
-              <LikeButton key={item.title} initialIsLiked={item.isLiked} />
-            </div>
-          </CardFooter>
+  return data.map((item, index) => (
+    <Link key={index} href={"list/1"}>
+      <ShadcnCard className="border-b-2 text-sm">
+        <CardHeader>
+          <CardTitle>{item.title}</CardTitle>
+        </CardHeader>
+        <div className="grid grid-cols-[1fr_120px] pb-4 gap-4">
+          <div className="flex flex-col justify-between">
+            <CardContent>
+              <div className="flex items-center gap-2">
+                <Button disabled={true} variant="outline" size="xs">
+                  {item.category}
+                </Button>
+                <p className="text-subText">{item.distance}</p>
+              </div>
+            </CardContent>
+            <CardFooter className="w-full pb-0">
+              <div className="flex items-center justify-between gap-2 w-full">
+                <p
+                  className={clsx("font-bold", {
+                    "text-green": item.isOpen,
+                    "text-red": !item.isOpen,
+                  })}
+                >
+                  {item.isOpen ? "営業中" : "営業時間外"}
+                  <br />
+                  <span className="text-text font-normal">
+                    {item.isOpen ? "営業終了" : "営業開始"}：{item.openingTime}
+                  </span>
+                </p>
+                <LikeButton key={item.title} initialIsLiked={item.isLiked} />
+              </div>
+            </CardFooter>
+          </div>
+          <div className="relative h-[120px] w-[120px]">
+            {!item.isOpen && (
+              <div className="absolute w-full h-full bg-gray-950/50 flex items-center justify-center">
+                <p className="text-white font-semibold text-md tracking-widest">
+                  営業時間外
+                </p>
+              </div>
+            )}
+            <Image
+              src={sample_soumen}
+              alt="sample_tyahan"
+              className="h-full object-cover object-center rounded-xl"
+            />
+          </div>
         </div>
-        <div className="relative h-[120px] w-[120px]">
-          {!item.isOpen && (
-            <div className="absolute w-full h-full bg-gray-950/50 flex items-center justify-center">
-              <p className="text-white font-semibold text-md tracking-widest">
-                営業時間外
-              </p>
-            </div>
-          )}
-          <Image
-            src={sample_soumen}
-            alt="sample_tyahan"
-            className="h-full object-cover object-center rounded-xl"
-          />
-        </div>
-      </div>
-    </ShadcnCard>
+      </ShadcnCard>
+    </Link>
   ));
 };
--- application/src/features/common/components/Card/index.tsx
+++ application/src/features/common/components/Card/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { clsx } from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import { CardDataType } from "../../type";
 import { Button } from "../Button";
 import { LikeButton } from "../LikeButton";
@@ -18,52 +19,51 @@ type Props = {
 };
 
 export const Card = ({ data }: Props) => {
-  return data.map((item) => (
-    <ShadcnCard
-      key={item.title}
-      className="border-2 w-[320px] shadow-md grid grid-rows-[240px_] rounded-lg"
-    >
-      <div className="relative">
-        {!item.isOpen && (
-          <div className="absolute w-full h-full bg-gray-950/50 flex items-center justify-center">
-            <p className="text-white font-semibold text-[20px] tracking-widest">
-              営業時間外
+  return data.map((item, index) => (
+    <Link key={index} href={"/list/1"}>
+      <ShadcnCard className="border-2 w-[320px] shadow-md grid grid-rows-[240px_] rounded-lg">
+        <div className="relative">
+          {!item.isOpen && (
+            <div className="absolute w-full h-full bg-gray-950/50 flex items-center justify-center">
+              <p className="text-white font-semibold text-[20px] tracking-widest">
+                営業時間外
+              </p>
+            </div>
+          )}
+          <Image
+            src={sample_soumen}
+            alt="sample_tyahan"
+            className="h-full object-cover object-center"
+          />
+        </div>
+        <div className="w-[320px] px-3">
+          <CardHeader>
+            <CardTitle>{item.title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center gap-2">
+              <Button disabled={true} variant="outline" size="sm">
+                {item.category}
+              </Button>
+              <p className="text-subText">{item.distance}</p>
+            </div>
+            <LikeButton key={item.title} initialIsLiked={item.isLiked} />
+          </CardContent>
+          <CardFooter>
+            <p
+              className={clsx("font-bold", {
+                "text-green": item.isOpen,
+                "text-red": !item.isOpen,
+              })}
+            >
+              {item.isOpen ? "営業中" : "営業時間外"}
+              <span className="pl-2 text-text font-normal">
+                {item.isOpen ? "営業終了" : "営業開始"}：{item.openingTime}
+              </span>
             </p>
-          </div>
-        )}
-        <Image
-          src={sample_soumen}
-          alt="sample_tyahan"
-          className="h-full object-cover object-center"
-        />
-      </div>
-      <div className="w-[320px] px-3">
-        <CardHeader>
-          <CardTitle>{item.title}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex items-center gap-2">
-            <Button disabled={true} variant="outline" size="sm">
-              {item.category}
-            </Button>
-            <p className="text-subText">{item.distance}</p>
-          </div>
-          <LikeButton key={item.title} initialIsLiked={item.isLiked} />
-        </CardContent>
-        <CardFooter>
-          <p
-            className={clsx("font-bold", {
-              "text-green": item.isOpen,
-              "text-red": !item.isOpen,
-            })}
-          >
-            {item.isOpen ? "営業中" : "営業時間外"}
-            <span className="pl-2 text-text font-normal">
-              {item.isOpen ? "営業終了" : "営業開始"}：{item.openingTime}
-            </span>
-          </p>
-        </CardFooter>
-      </div>
-    </ShadcnCard>
+          </CardFooter>
+        </div>
+      </ShadcnCard>
+    </Link>
   ));
 };
