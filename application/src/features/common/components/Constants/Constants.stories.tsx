c0377e0ca36244daac5799d804220531891060ca
kaneko1117 <yuta_kaneko@pj100.biz>
2024-07-13 14:01:26 +0900

    ✨コンスタンスコンポーネントを実装

---
+++ application/src/features/common/components/Constants/Constants.stories.tsx
@@ -0,0 +1,18 @@
+import { Constants as Component } from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  component: Component,
+  args: {
+    title: "美味しい餃子のお店あああ",
+    category: "カフェ",
+    openingTime: "10:00-20:00",
+    phone: "000-0000-0000",
+    regularHoliday: "水曜日",
+    address: "東京都渋谷区1-22",
+  },
+} as Meta<typeof Component>;
+
+type Story = StoryObj<typeof Component>;
+
+export const Default: Story = {};
---
+++ application/src/features/common/components/Constants/index.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+import { Clock, Phone, CalendarX, MapPin, FilePenLine } from "lucide-react";
+import { Button } from "../Button";
+
+type SubTextProps = {
+  children: ReactNode;
+  text: string;
+};
+
+const SubText = ({ children, text }: SubTextProps) => {
+  return (
+    <div className="flex items-center w-fit">
+      {children}
+      <p className="pl-2">{text}</p>
+    </div>
+  );
+};
+
+type Props = {
+  title: string;
+  category: string;
+  openingTime: string;
+  phone: string;
+  regularHoliday: string;
+  address: string;
+};
+
+export const Constants = ({
+  title,
+  category,
+  openingTime,
+  phone,
+  regularHoliday,
+  address,
+}: Props) => {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="text-text text-2xl md:text-3xl font-bold">{title}</h1>
+        <div className="flex items-center w-fit">
+          <p className="text-text w-[32px]">編集</p>
+          <FilePenLine className="h-5 w-5 text-primary" />
+        </div>
+      </div>
+      <Button variant="outline" className="my-4">
+        {category}
+      </Button>
+      <div className="text-subText block md:grid grid-cols-[max-content_auto] gap-x-4">
+        <SubText text={openingTime}>
+          <Clock className="h-5 w-5" />
+        </SubText>
+        <SubText text={phone}>
+          <Phone className="h-5 w-5" />
+        </SubText>
+        <SubText text={regularHoliday}>
+          <CalendarX className="h-5 w-5" />
+        </SubText>
+        <SubText text={address}>
+          <MapPin className="h-5 w-5" />
+        </SubText>
+      </div>
+    </div>
+  );
+};
