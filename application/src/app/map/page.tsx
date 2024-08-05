4663cd952bd69145b95b70cfbdd92c2361a119fb
kaneko1117 <yuta_kaneko@pj100.biz>
2024-08-05 22:17:49 +0900

    ✨ディレクトリ構成を修正

--- application/src/app/map/page.tsx
+++ application/src/app/map/page.tsx
@@ -1,5 +1,5 @@
-import { MapTemplate } from "./MapTemplate";
 import { getRestaurants } from "@/src/data/restaurants";
+import { MapTemplate } from "@/src/features/common/components/Templates/Map";
 
 export default async function Map() {
   const data = await getRestaurants();
